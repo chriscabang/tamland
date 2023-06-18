22117313349d1d86b54c290804db41cf85edef45
Chris Cabang <ccscabang@gmail.com>
2023-06-18 15:34:06 +0100

    refactor: move tamland to main

--- src/main.py
+++ src/main.py
@@ -1,80 +1,37 @@
-import RPi.GPIO as GPIO
-import board
-import busio
-# import Adafruit_DHT
-import adafruit_dht     # pip3 install adafruit-circuitpython-dht
-import adafruit_sgp30   # pip3 install adafruit-circuitpython-sgp30 
+import socket
 
-from time import sleep
+# Server IP address and port
+server_ip = 'localhost'  # or '127.0.0.1'
+server_port = 9980  # Port number to listen on
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c, 0x58)
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("SGP30 serial #", [hex(i) for i in sgp30.serial])
+# Bind the socket to the IP address and port
+server_socket.bind((server_ip, server_port))
 
-sgp30.set_iaq_baseline(0x8973, 0x8AAE)
-sgp30.set_iaq_relative_humidity(celsius=22.1, relative_humidity=44)
+# Listen for incoming connections
+server_socket.listen()
 
-dht = adafruit_dht.DHT11(board.D10)
+print(f"Server listening on {server_ip}:{server_port}")
 
-BUZZER  = 9
-LED     = 11
-#LIGHT   = 17
-#signal  = 0
-
-#DHT11   = dht11()
-
-def init():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setwarnings(False)
-  GPIO.setup(BUZZER , GPIO.OUT)
-  GPIO.setup(LED    , GPIO.OUT)
-#  GPIO.setup(LIGHT  , GPIO.IN )
-#  signal = not GPIO.input(LIGHT)
-  return
-
-def main():
+while True:
   try:
-    elapsed_sec = 0
-    init()
-#    global signal
+    # Accept a client connection
+    client_socket, client_address = server_socket.accept()
+    #print(f"Accepted connection from {client_address}")
 
-    while True:
-#      if GPIO.input(LIGHT) != signal:
-#        if not GPIO.input(LIGHT):
-#          print('\u263e')   # dark
-#        else:
-#          print('\u263c')   # light
-#      signal = GPIO.input(LIGHT)
+    # Receive data from the client
+    data = client_socket.recv(1024).decode()
+    print(f"Received data: {client_address} {data}")
 
-#      humidity, temperature = DHT11.read()
-      try:
-        temperature = dht.temperature
-        humidity    = dht.humidity
-        if humidity is not None and temperature is not None:
-          print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
-      except RuntimeError as error:
-        sleep(0.5)
-        continue
+    # Process the received data or perform other operations
 
-      print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
-      elapsed_sec += 1
-      if elapsed_sec > 10:
-        elapsed_sec = 0
-        print(
-          "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
-          % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
-        )
+    # Send a response back to the client
+    #response = "Hello, client!"
+    #client_socket.sendall(response.encode())
 
-      GPIO.output(BUZZER, GPIO.LOW)
-      GPIO.output(LED, GPIO.LOW)
-      sleep(1.5)
-      GPIO.output(BUZZER, GPIO.HIGH)
-      GPIO.output(LED, GPIO.HIGH)
-      sleep(0.5)
+    # Close the client socket
+    client_socket.close()
   except KeyboardInterrupt:
-    GPIO.cleanup()
     exit(1)
-
-if __name__ == "__main__":
-  main()
